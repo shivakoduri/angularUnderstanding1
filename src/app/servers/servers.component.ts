21a40107e1e17eafffcbe70c05cc93814040aa6f
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-05-09 22:47:04 +0530

    commit for component selector, selecting elemeny by attribute

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: '[app-servers]', //selects element by attribute, not by element itself
   template: '<app-server></app-server> <app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
