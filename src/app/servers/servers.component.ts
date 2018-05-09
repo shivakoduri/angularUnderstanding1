01cf9fcf6a25e6f32f93709fe6721f22c7d79abf
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-05-09 22:49:04 +0530

    commit for component selector, selecting elemeny by class

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-servers]', //selects element by attribute, not by element itself
+  // selector: '[app-servers]', //selects element by attribute, not by element itself
+  selector : '.app-servers', //select elemeny be class
   template: '<app-server></app-server> <app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
