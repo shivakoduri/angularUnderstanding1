b2fe7c79522f2a7afda430f2049a87a9e2c16c9b
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-05-10 20:13:34 +0530

    commit for databinding string interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   // selector: '[app-servers]', //selects element by attribute, not by element itself
-  selector : '.app-servers', //select elemeny be class
-  template: '<app-server></app-server> <app-server></app-server>',
+  // selector : '.app-servers', //select elemeny be class
+  selector: 'app-servers',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+    `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
