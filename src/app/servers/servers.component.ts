5fcfec3014ea0c056404f014b2c8491458246325
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-05-10 20:24:33 +0530

    commit for databinding - property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,15 +4,22 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]', //selects element by attribute, not by element itself
   // selector : '.app-servers', //select elemeny be class
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-    `,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  //   `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewSever = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewSever = true;
+    },2000);
+  }
 
   ngOnInit() {
   }
